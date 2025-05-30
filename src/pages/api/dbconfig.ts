78e50ccdaae3bfafa4d41f99d25259ca9b8cf281
karlokolac <155363298+karlokolac@users.noreply.github.com>
2025-05-30 12:42:55 +0200

    Refactor production dbConfig in dbConfig.ts

--- src/pages/api/dbconfig.ts
+++ src/pages/api/dbconfig.ts
@@ -1,15 +1,5 @@
 // production
-export const dbConfig = {
-  host: import.meta.env.AIVEN_DB_HOST,
-  user: import.meta.env.AIVEN_DB_USER,
-  password: import.meta.env.AIVEN_DB_PASSWORD,
-  database: import.meta.env.AIVEN_DB_NAME,
-  port: parseInt(import.meta.env.AIVEN_DB_PORT),
-  ssl: {
-    ca: import.meta.env.AIVEN_DB_CA_CERT,
-    rejectUnauthorized: true,
-  },
-};
+export const dbConfig = import.meta.env.AIVEN_DB_URL;
 
 // testing
 // export const dbConfig = {
