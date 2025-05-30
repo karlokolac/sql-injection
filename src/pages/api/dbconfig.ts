3dd229a2b7154c9bc58109140091b423047a0e13
karlokolac <155363298+karlokolac@users.noreply.github.com>
2025-05-30 12:26:09 +0200

    Refactor dbConfig to use production environment variables

--- src/pages/api/dbconfig.ts
+++ src/pages/api/dbconfig.ts
@@ -1,20 +1,20 @@
 // production
-// export const dbConfig = {
-//   host: import.meta.env.AIVEN_DB_HOST,
-//   user: import.meta.env.AIVEN_DB_USER,
-//   password: import.meta.env.AIVEN_DB_PASSWORD,
-//   database: import.meta.env.AIVEN_DB_NAME,
-//   port: parseInt(import.meta.env.AIVEN_DB_PORT),
-//   ssl: {
-//     ca: import.meta.env.AIVEN_DB_CA_CERT,
-//     rejectUnauthorized: true,
-//   },
-// };
+export const dbConfig = {
+  host: import.meta.env.AIVEN_DB_HOST,
+  user: import.meta.env.AIVEN_DB_USER,
+  password: import.meta.env.AIVEN_DB_PASSWORD,
+  database: import.meta.env.AIVEN_DB_NAME,
+  port: parseInt(import.meta.env.AIVEN_DB_PORT),
+  ssl: {
+    ca: import.meta.env.AIVEN_DB_CA_CERT,
+    rejectUnauthorized: true,
+  },
+};
 
 // testing
-export const dbConfig = {
-  host: import.meta.env.LOCAL_DB_HOST,
-  user: import.meta.env.LOCAL_DB_USER,
-  password: import.meta.env.LOCAL_DB_PASSWORD,
-  database: import.meta.env.LOCAL_DB_NAME,
-};
+// export const dbConfig = {
+//   host: import.meta.env.LOCAL_DB_HOST,
+//   user: import.meta.env.LOCAL_DB_USER,
+//   password: import.meta.env.LOCAL_DB_PASSWORD,
+//   database: import.meta.env.LOCAL_DB_NAME,
+// };
